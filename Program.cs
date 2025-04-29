18a775f6f8fd641d62b23425191f3d1e85e96760
simgaoski <oskar-o3@o2.pl>
2025-04-29 13:48:01 +0200

    Add files via upload
    testowa 1.0

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Podaj liczbę n: ");
+            int n = int.Parse(Console.ReadLine());
+            int wynik = n * n + 1;
+            Console.WriteLine($"Wynik n^2 + 1 to: {wynik}");
+        }
+    }
+}
